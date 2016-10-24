28ab589db303fff7e53092b00dd3758bdfcd6f7b
martinjhnhadley <martinjohnhadley@gmail.com>
2016-10-24 11:00:15 +0100

    Fixed = instead of <- assignment
    HT Maja

--- Handouts/htmlwidgets.R
+++ Handouts/htmlwidgets.R
@@ -1,7 +1,7 @@
 ## === Worked example
 
 ## Africa data points
-africa_data_points = data.frame(
+africa_data_points <- data.frame(
   lat = rnorm(26, mean = 6.9, sd = 10),
   lng = rnorm(26, mean = 17.7, sd = 10),
   size = runif(26, 5, 10),
